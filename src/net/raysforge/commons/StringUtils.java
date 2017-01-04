c31c5eeca7e3ad2fd7ab1030b3beae74a5e09266
Raymond Hulha <java4life@gmail.com>
2017-01-04 13:32:37 +0100

    Added hasLength, join(String, List) and substring(String, char, char) to StringUtils

--- src/net/raysforge/commons/StringUtils.java
+++ src/net/raysforge/commons/StringUtils.java
@@ -1,7 +1,13 @@
 package net.raysforge.commons;
 
+import java.util.List;
+
 public class StringUtils {
 	
+	public static boolean hasLength(String text) {
+		return (text != null && text.length() > 0);
+	}
+
 	public static String join(String con, String args[]) {
 		if (args.length == 0)
 			return "";
@@ -14,4 +20,21 @@ public class StringUtils {
 		return sb.toString();
 	}
 	
+	public static String join(String con, List<String> args) {
+		if (args.size() == 0)
+			return "";
+		StringBuffer sb = new StringBuffer();
+		sb.append(args.get(0));
+		for (int n = 1; n < args.size(); n++) {
+			sb.append(con);
+			sb.append(args.get(n));
+		}
+		return sb.toString();
+	}
+
+	public static String substring(String str, char a, char b)
+    {
+        return str.substring(str.indexOf(a) + 1, str.indexOf(b));
+    }
+
 }
