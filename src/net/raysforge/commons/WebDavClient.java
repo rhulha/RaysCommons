699118325f53b949bf595388c5fed8b160d9c9d1
Ray <java4life@gmail.com>
2022-09-07 11:50:00 +0200

    Switched the WebDavClient from java.net.HttpURLConnection to java.net.http.HttpClient

--- src/net/raysforge/commons/WebDavClient.java
+++ src/net/raysforge/commons/WebDavClient.java
@@ -1,179 +1,173 @@
 package net.raysforge.commons;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Authenticator;
-import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.Base64;
 
-public class WebDavClient extends Authenticator {
+public class WebDavClient {
 	
 	private final String baseURL;
-	private final String username;
-	private final String password;
-	private boolean isAuthenticated = false;
+	private final HttpClient httpClient;
+
+    //String userpass = username + ":" + password;
+    //String basicAuth = "Basic " + Base64.getEncoder().encodeToString(userpass.getBytes());
+    //c.setRequestProperty ("Authorization", basicAuth);          
 
 	public WebDavClient(String baseURL, String username, String password) {
 		this.baseURL = baseURL;
-		this.username = username;
-		this.password = password;
-		Authenticator.setDefault(this);
-	}
-	
-	public PasswordAuthentication getPasswordAuthentication() {
-		isAuthenticated = true;
-		return new PasswordAuthentication(username, password.toCharArray());
-	}
-	
-	public boolean exists(String urlPath) throws IOException {
-		System.out.println("checking: " + urlPath);
-
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		c.setRequestMethod("HEAD");
-
-		int responseCode = c.getResponseCode();
-		return responseCode != 404;
-
-	}
-	
-    public void createFolder(String urlPath) throws Exception {
-        System.out.println("createFolder: " + urlPath);
-        
-        HttpClient httpClient = HttpClient.newBuilder()
+        httpClient = HttpClient.newBuilder()
                 .authenticator(new Authenticator() {
                     @Override
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(username, password.toCharArray());
                     }
                 })
-                .version(HttpClient.Version.HTTP_1_1)
+                //.version(HttpClient.Version.HTTP_1_1)
                 .connectTimeout(Duration.ofSeconds(10))
                 .build();
-        
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(baseURL + urlPath))
-                .method("MKCOL", HttpRequest.BodyPublishers.ofString(""))
-                //.header("Content-Type", "text/xml")
-                .build();
+	}
+	
+    private HttpResponse<String> request1(String method, String urlPath) throws IOException {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + urlPath)).method(method, HttpRequest.BodyPublishers.ofString("")).build();
+        //.header("Content-Type", "text/xml")
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(e);
+        }
+    }
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    protected String request2(String method, String urlPath) throws IOException {
+        HttpResponse<String> response = request1(method,urlPath);
+        int sc = response.statusCode();
+        if( sc >= 200 && sc < 300 ) {
+            return response.body();
+        } else {
+            throw new IOException("HTTP ERROR: " + sc);
+        }
+	}
+
+    public boolean exists(String urlPath) throws IOException {
+		System.out.println("checking: " + urlPath);
+		HttpResponse<String> response = request1("HEAD", urlPath);
+		return response.statusCode() != 404;
+	}
+	
+    public boolean createFolder(String urlPath) throws Exception {
+        System.out.println("createFolder: " + urlPath);
         
-        if( response.statusCode() != 201 )
+        if(exists(urlPath)) {
+            return false;
+        }
+        
+        HttpResponse<String> response = request1("MKCOL", urlPath);
+        
+        int sc = response.statusCode();
+        if( sc != 201 )  {
             System.out.println(response.body());
-
+            throw new IOException("HTTP ERROR: " + sc);
+        } else {
+            return true;
+        }
     }
 
 	public boolean delete(String urlPath) throws IOException {
 		System.out.println("deleting: " + urlPath);
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		c.setRequestMethod("DELETE");
-
-		int responseCode = c.getResponseCode();
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Copy.streamAndClose(c.getInputStream(), baos);
-
-		//System.out.println(baos.toString());
-		return responseCode == 204;
+		HttpResponse<String> response = request1("DELETE", urlPath);
+        int sc = response.statusCode();
+		return sc == 204;
 	}
 
-	public HttpURLConnection getUploadConnection(String urlPath) throws IOException {
+    public boolean upload(InputStream is, String urlPath) throws IOException {
+        System.out.println("uploading: " + urlPath);
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseURL + urlPath))
+                .headers("Content-Type", "text/plain;charset=UTF-8")
+                .PUT(HttpRequest.BodyPublishers.ofInputStream(()->is))
+                .build();
+        
+        try {
+            HttpResponse<String> response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            int sc = response.statusCode();
+            if( sc >= 200 && sc < 300 ) {
+                return sc == 201;
+            } else {
+                throw new IOException("HTTP ERROR: " + sc);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(e);
+        }
+
+    }
+
+    public boolean upload(String localFile, String urlPath) throws IOException {
 		System.out.println("uploading: " + urlPath);
-
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		ensureAuthentication(c);
-		c.setChunkedStreamingMode(65536);
-		c.setDoInput(true);
-		c.setDoOutput(true);
-
-		c.setRequestMethod("PUT");
-		return c;
-	}
-	
-	public void assertResponseCode(HttpURLConnection c, int code) throws IOException
-	{
-		int responseCode = c.getResponseCode();
-
-		InputStream is = c.getInputStream(); // eat response data.
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Copy.streamAndClose(is, baos);
-
-		//System.out.println(baos.toString());
-		if( responseCode != code)
-			throw new RuntimeException("responseCode != code: " + responseCode +" != "+code);
-	}
-	
-	public boolean upload(String localFile, String urlPath) throws IOException {
-		//System.out.println("uploading: " + urlPath);
 		
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		ensureAuthentication(c);
-		c.setChunkedStreamingMode(65536);
-		c.setDoInput(true);
-		c.setDoOutput(true);
+		HttpRequest request = HttpRequest.newBuilder()
+		        .uri(URI.create(baseURL + urlPath))
+		        .headers("Content-Type", "text/plain;charset=UTF-8")
+		        .PUT(HttpRequest.BodyPublishers.ofFile(Paths.get(localFile)))
+		        .build();
+		
+        try {
+            HttpResponse<String> response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            int sc = response.statusCode();
+            if( sc >= 200 && sc < 300 ) {
+                return sc == 201;
+            } else {
+                throw new IOException("HTTP ERROR: " + sc);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(e);
+        }
 
-		c.setRequestMethod("PUT");
-
-		OutputStream os = c.getOutputStream();
-		FileInputStream fis = new FileInputStream(localFile);
-		Copy.streamAndClose(fis, os);
-
-		int responseCode = c.getResponseCode();
-
-		InputStream is = c.getInputStream();
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		Copy.streamAndClose(is, baos);
-
-		//System.out.println(baos.toString());
-		return responseCode == 201;
 	}
 
-	private void ensureAuthentication(HttpURLConnection c) {
-		if( !isAuthenticated)
-		{
-			String userpass = username + ":" + password;
-			String basicAuth = "Basic " + Base64.getEncoder().encodeToString(userpass.getBytes());
-			c.setRequestProperty ("Authorization", basicAuth);			
-		}
-	}
-
-	public void download(String urlPath, String localFile) throws IOException {
+	public void download(String urlPath, String localFile) throws IOException  {
 		System.out.println("downloading: " + urlPath);
 
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		InputStream is = c.getInputStream();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + urlPath)).build();
 
-		FileOutputStream fos = new FileOutputStream(localFile);
-		Copy.streamAndClose(is, fos);
+        try {
+            this.httpClient.send(request, HttpResponse.BodyHandlers.ofFile(Path.of(localFile)));
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(e);
+        }
 	}
 
 	public InputStream getDownloadStream(String urlPath) throws IOException {
 		System.out.println("downloading: " + urlPath);
 
-		URL u = new URL(baseURL + urlPath);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
-		return c.getInputStream();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + urlPath)).build();
+
+        try {
+            HttpResponse<InputStream> response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+            int sc = response.statusCode();
+            if( sc >= 200 && sc < 300 ) {
+                return response.body();
+            } else {
+                throw new IOException("HTTP ERROR: " + sc);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(e);
+        }
 	}
 
-	public void deleteIfItExists(String urlPath) throws IOException {
-		if( exists(urlPath))
-			delete(urlPath);
+	public boolean deleteIfItExists(String urlPath) throws IOException {
+		if( exists(urlPath)) {
+			return delete(urlPath);
+		} else {
+		    return false;
+		}
 		
 	}
 
